375ddbd75ea6cf95e244f1e90363af3d14e82286
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-16 16:04:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,41 +6,34 @@ import joblib
 
 st.image("logo.png", width=450)
 st.title("PREDICT A CARD'S LEVEL")
-#col1, col2, col3 = st.columns(3)
-#with col1:
-    #st.image("yugi.png")
-#with col2:
-    st.header('Enter type of the card')
-    typec = st.selectbox('',("Normal Monster","Spell Card", "Effect Monster","Trap Card"))
-    st.write('You selected:', typec)
-    if typec in ("Spell Card") :
-      st.subheader("Spell cards they have no level")
-    elif typec in ("Trap Card") :
-      st.subheader("Trap cards they have no level")
-    else:
+st.header('Enter type of the card')
+typec = st.selectbox('',("Normal Monster","Spell Card", "Effect Monster","Trap Card"))
+st.write('You selected:', typec)
+if typec in ("Spell Card") :
+	st.subheader("Spell cards they have no level")
+elif typec in ("Trap Card") :
+	st.subheader("Trap cards they have no level")
+else:
 
-        # Add a atk input
-        st.header('Attack Points of the card:')
-        atk = st.number_input('')
+    # Add a atk input
+    st.header('Attack Points of the card:')
+    atk = st.number_input('')
 
-        # Add a def input
-        st.header("Defense Points of the card:")
-        defn = st.number_input("",key="def")
+    # Add a def input
+    st.header("Defense Points of the card:")
+    defn = st.number_input("",key="def")
 
 
-        # Display the entered name
-        if st.button("Enviar"):
+    # Display the entered name
+    if st.button("Enviar"):
 
-            level_model = joblib.load("level_model.pkl")
+        level_model = joblib.load("level_model.pkl")
 
-            X = pd.DataFrame([[typec,atk, defn ]],
-                columns = ["type","atk", "def"])
-            X = X.replace(["Normal Monster","Spell Card", "Effect Monster","Trap Card"], [0.,1.,2.,3.])
+        X = pd.DataFrame([[typec,atk, defn ]],
+            columns = ["type","atk", "def"])
+        X = X.replace(["Normal Monster","Spell Card", "Effect Monster","Trap Card"], [0.,1.,2.,3.])
 
 
-            prediction = level_model.predict(X)[0]
+        prediction = level_model.predict(X)[0]
 
-            st.text(f"Es un {round(prediction)}")
-
-#with col3:
-    #st.image("kaiba.png")
+        st.text(f"Es un {round(prediction)}")
