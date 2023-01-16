f4662d0e6a2f001ba77f97e123cda063ba52f057
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-16 14:43:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,16 @@ import joblib
 
 st.title("Nivel de la carta")
 
-typec = st.selectbox('Enter type?',("Normal Monster","Spell Card", "Effect Monster","Trap Card"))
+typec = st.selectbox('Enter type of the card',("Normal Monster","Spell Card", "Effect Monster","Trap Card"))
 st.write('You selected:', typec)
+if typec == "Spell Card"
+	st.text("NO tiene nivel")
 
 # Add a atk input
-atk = st.number_input("Enter atk:")
+atk = st.number_input('Attack Points of the card:')
 
 # Add a def input
-defn = st.number_input("Enter defn:")
+defn = st.number_input("Enter Defense Points of the card:")
 
 
 # Display the entered name
@@ -23,6 +25,8 @@ if st.button("Enviar"):
 
     X = pd.DataFrame([[typec,atk, defn ]],
 			   columns = ["type","atk", "def"])
+    X = X.replace(["Normal Monster","Spell Card", "Effect Monster","Trap Card"], [0.,1.,2.,3.])
+
 
     prediction = level_model.predict(X)[0]
 
