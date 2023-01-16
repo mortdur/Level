98d2242db86fa6441e8fd24568c0c563934a6a2c
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-16 17:34:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ else:
         prediction = level_model.predict(X)[0]
 
         st.subheader(f"The most appropriate level for this card is {round(prediction)}")
-st.text("hola {"*" * prediction}")
+st.text("hola {print("*" * prediction)}")
