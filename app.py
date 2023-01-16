996b77d13084c1da9b796fac645c5dd562850f2a
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-16 17:11:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,12 +17,12 @@ else:
     # Add a atk input
     st.header('Attack Points of the card:')
     #atk = st.number_input('')
-    atk = st.slider("ATK:",0, 5000)
+    atk = st.slider("ATK:",0.0,100.0, 5000.0)
 
     # Add a def input
     st.header("Defense Points of the card:")
     #defn = st.number_input("",key="def")
-    defn = st.slider("DEF:",0, 5000,key="def")
+    defn = st.slider("DEF:",0.0,100.0, 5000.0,key="def")
 
     # Display the entered name
     if st.button("Enviar"):
