2a4a3b9a4c3cd7bed814b6de2e7b81701b084c3a
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-17 20:56:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ font-size: 20px;
 font-weight: 400;
 }
 
-div class="st-c7 st-be st-c8 st-c9 st-ca st-bg st-cb st-cc st-cd" {
+.st-c7 st-be st-c8 st-c9 st-ca st-bg st-cb st-cc st-cd {
 color : rgb(189, 0, 0);
 }
 
