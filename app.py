cb678f83aa5ad508ad02fd93e82cbb7e550159f9
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-17 08:30:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ page_bg_img = """
 <style>
 [data-testid="stAppViewContainer"] {
 background-color: #e5e5f7;
-opacity: 0.7;
+opacity: 0.8;
 background: linear-gradient(135deg, #27295955 25%, transparent 25%) -20px 0/ 40px 40px, linear-gradient(225deg, #272959 25%, transparent 25%) -20px 0/ 40px 40px, linear-gradient(315deg, #27295955 25%, transparent 25%) 0px 0/ 40px 40px, linear-gradient(45deg, #272959 25%, #e5e5f7 25%) 0px 0/ 40px 40px;
 }
 
