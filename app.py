05910e20743b93b8c7a3838841a420e6856c7138
mortdur <121159231+mortdur@users.noreply.github.com>
2023-01-17 12:46:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ background: rgba(0,0,0,0);
 color : rgb(0, 0, 0);
 background: rgba(0, 0, 0, 0.15);
 border-radius: 15px 50px;
-padding: cover;
+padding: 20px 15 px;
 }
 [data-testid=".st-br"]{
 color : rgb(0, 0, 0);
